e35f5e43921102358f9942c4346733dbe950b035
Hung2201 <duchung12a8@gmail.com>
2024-03-11 09:39:02 +0700

    finish bai26

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,8 +47,10 @@ class HomeController extends Controller
     public function postAdd(Request $request)
     {
         $rule = [
-            'product_name' => ['required','min:6', new Uppercase],
-            'product_price' => ['required','integer', new Uppercase]
+            'product_name' => ['required','min:6', function($attribute, $value, $fail){
+                isUppercase($value, 'Trường :attribute không hợp lệ', $fail);
+            }],
+            'product_price' => ['required','integer']
         ];
         // $message =
         // [
